38453e433cedd1222feb214f8a5063ae9b7c859b
gyf1214 <gyf1214@gmail.com>
2016-04-26 22:07:52 +0800

    finish gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,26 +6,45 @@ var gulp = require('gulp'),
   clean = require('gulp-clean'),
   concat = require('gulp-concat'),
   rename = require('gulp-rename'),
-  util = require('gulp-util');
+  util = require('gulp-util'),
+  merge = require('merge-stream');
 
-var srcFile = 'src/*.coffee',
-  target = 'main.js',
-  tarPath = 'bin'
+var libFile = ['src/*.coffee', '!src/main.coffee'],
+  mainFile = 'src/main.coffee',
+  libTarget = 'lib.js',
+  mainTarget = 'main.js',
+  tarPath = 'bin';
 
-gulp.task('coffee', function () {
-  return gulp.src(srcFile)
+var coffeefy = function(src, target) {
+  var d = src
     .pipe(lint())
     .pipe(lint.reporter())
     .pipe(cache(coffee()))
     .on('error', function (err) {
       this.emit('end');
     })
-    .pipe(concat(target))
-    .pipe(gulp.dest(tarPath))
+    if (target !== null && target !== undefined) {
+      return d
+        .pipe(concat(target))
+        .pipe(gulp.dest(tarPath));
+    } else return d;
+}
+
+var compress = function(src) {
+  return src
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify({output: {max_line_len: 64}}))
     .on('error', util.log)
     .pipe(gulp.dest(tarPath));
+}
+
+gulp.task('coffee', function () {
+  var main = gulp.src(mainFile),
+    lib = gulp.src(libFile);
+
+  return compress(merge(coffeefy(main), coffeefy(lib, libTarget))
+          .pipe(concat(mainTarget))
+          .pipe(gulp.dest(tarPath)));
 });
 
 gulp.task('watch', function (){
